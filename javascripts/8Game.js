775140a00165f91dde902bd55c527ab90374cada
Ester-Martinez <martinez.ester@gmail.com>
2019-09-06 12:50:25 +0200

    con fin de juego

---
+++ javascripts/8Game.js
@@ -0,0 +1,37 @@
+// window.onload = function() {
+//   startGame();
+// };
+window.onload = function() {
+  coverImage();
+};
+const coverImg = new Image();
+coverImg.src = "./Images/logo1.png";
+const endImg = new Image();
+endImg.src = "./Images/We-have-a-winner.png"
+
+snake0.placeSnake();
+snake1.placeSnake();
+ladder0.placeLadder();
+ladder1.placeLadder();
+redPawn.resetPawn();
+yellowPawn.resetPawn();
+die.placeDie();
+function startGame() {
+  let counter = 0;
+  this.intervalID = setInterval(() => {
+    resetCanvas();
+    drawGameSpace();
+    userControls();
+    drawPawns();
+    die.drawDie();
+    checkPositionActivePlayer();
+
+    if (redPawn.currentPos === 45 || yellowPawn.currentPos === 45) {
+      clearInterval(intervalID);
+      resetCanvas();
+      ctx.drawImage(endImg, 0, 0, w, h);
+      ;
+    }
+    counter++;
+  }, 1000 / 60);
+}
